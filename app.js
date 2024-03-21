cc940f84dd318d1c140bd4a4a4b36ab883787c01
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-03-21 15:04:23 +0530

    BMI calculator project

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const btn = document.querySelector("#btn");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const weight = parseInt(document.querySelector("#weight").value);
+  const height = parseInt(document.querySelector("#height").value);
+  const result = document.querySelector("#ans");
+    
+  if(weight==='' || weight<0 || isNaN(weight)){
+    result.innerText = "error";
+  }
+  if(height==='' || height<50 || height> 300 || isNaN(height)){
+    result.innerText = "error";
+  }
+
+  else{
+    result.innerText = (weight/((height*height)/10000)).toFixed(2)
+  }
+  console.log(`Weight = ${weight}`);
+  console.log(`height = ${height}`);
+  console.log(`BMI = ${result.innerText}`);
+  // result.innerText = `${weight/height}`
+})
+
+
